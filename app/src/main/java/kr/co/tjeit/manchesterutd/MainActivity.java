6480a332b69dbb97809ed62efd0d02730e4ba7cc
pyj2001sus <pyj2001sus@hanmail.net>
2017-10-18 10:54:27 +0900

    로그인, 회원가입, 회원정보수정 등등 기반확립

--- app/src/main/java/kr/co/tjeit/manchesterutd/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/manchesterutd/MainActivity.java
@@ -1,8 +1,10 @@
 package kr.co.tjeit.manchesterutd;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import kr.co.tjeit.manchesterutd.util.ContextUtil;
@@ -17,6 +19,10 @@ public class MainActivity extends BaseActivity {
     private ImageView newsBtn;
     private ImageView scaduleBtn;
     private ImageView memberBtn;
+    private android.widget.FrameLayout homeFragment;
+    private android.widget.FrameLayout newsFragment;
+    private android.widget.FrameLayout scaduleFragment;
+    private android.widget.FrameLayout memberFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +35,80 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-        myProfileBtn.setOnClickListener(new View.OnClickListener() {
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (ContextUtil.getLoginUser(mContext) == null){
-                    Intent intent = new Intent(mContext, LoginActivity.class);
-                    startActivity(intent);
-                }
-                else {
-                    Intent intent = new Intent(mContext, MyProfileActivity.class);
-                    startActivity(intent);
-                }
-
+                homeFragment.setVisibility(View.VISIBLE);
+                newsFragment.setVisibility(View.GONE);
+                scaduleFragment.setVisibility(View.GONE);
+                memberFragment.setVisibility(View.GONE);
+                homeBtn.setScaleX(1.2f);
+                homeBtn.setScaleY(1.2f);
+                newsBtn.setScaleX(1);
+                newsBtn.setScaleY(1);
+                scaduleBtn.setScaleX(1);
+                scaduleBtn.setScaleY(1);
+                memberBtn.setScaleX(1);
+                memberBtn.setScaleY(1);
             }
         });
+
+        newsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeFragment.setVisibility(View.GONE);
+                newsFragment.setVisibility(View.VISIBLE);
+                scaduleFragment.setVisibility(View.GONE);
+                memberFragment.setVisibility(View.GONE);
+                homeBtn.setScaleX(1);
+                homeBtn.setScaleY(1);
+                newsBtn.setScaleX(1.2f);
+                newsBtn.setScaleY(1.2f);
+                scaduleBtn.setScaleX(1);
+                scaduleBtn.setScaleY(1);
+                memberBtn.setScaleX(1);
+                memberBtn.setScaleY(1);
+            }
+        });
+
+        scaduleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeFragment.setVisibility(View.GONE);
+                newsFragment.setVisibility(View.GONE);
+                scaduleFragment.setVisibility(View.VISIBLE);
+                memberFragment.setVisibility(View.GONE);
+                homeBtn.setScaleX(1);
+                homeBtn.setScaleY(1);
+                newsBtn.setScaleX(1);
+                newsBtn.setScaleY(1);
+                scaduleBtn.setScaleX(1.2f);
+                scaduleBtn.setScaleY(1.2f);
+                memberBtn.setScaleX(1);
+                memberBtn.setScaleY(1);
+            }
+        });
+
+        memberBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeFragment.setVisibility(View.GONE);
+                newsFragment.setVisibility(View.GONE);
+                scaduleFragment.setVisibility(View.GONE);
+                memberFragment.setVisibility(View.VISIBLE);
+                homeBtn.setScaleX(1);
+                homeBtn.setScaleY(1);
+                newsBtn.setScaleX(1);
+                newsBtn.setScaleY(1);
+                scaduleBtn.setScaleX(1);
+                scaduleBtn.setScaleY(1);
+                memberBtn.setScaleX(1.2f);
+                memberBtn.setScaleY(1.2f);
+            }
+        });
+
+
     }
 
     @Override
@@ -58,5 +123,9 @@ public class MainActivity extends BaseActivity {
         this.scaduleBtn = (ImageView) findViewById(R.id.scaduleBtn);
         this.newsBtn = (ImageView) findViewById(R.id.newsBtn);
         this.homeBtn = (ImageView) findViewById(R.id.homeBtn);
+        this.memberFragment = (FrameLayout) findViewById(R.id.memberFragment);
+        this.scaduleFragment = (FrameLayout) findViewById(R.id.scaduleFragment);
+        this.newsFragment = (FrameLayout) findViewById(R.id.newsFragment);
+        this.homeFragment = (FrameLayout) findViewById(R.id.homeFragment);
     }
 }

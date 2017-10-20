4b260b18b4cff969e2f0066e0887ea96b7c7a32a
pyj2001sus <pyj2001sus@hanmail.net>
2017-10-20 17:45:30 +0900

    퍼스트팀 선수 상세보기 작업중

--- app/src/main/java/kr/co/tjeit/manchesterutd/adapter/FirstTeamAdapter.java
+++ app/src/main/java/kr/co/tjeit/manchesterutd/adapter/FirstTeamAdapter.java
@@ -2,6 +2,7 @@ package kr.co.tjeit.manchesterutd.adapter;
 
 import android.content.Context;
 import android.nfc.Tag;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -17,6 +18,7 @@ import java.util.List;
 
 import kr.co.tjeit.manchesterutd.R;
 import kr.co.tjeit.manchesterutd.data.member.FirstTeam;
+import kr.co.tjeit.manchesterutd.util.GlobalData;
 
 /**
  * Created by the on 2017-10-20.
@@ -58,7 +60,7 @@ public class FirstTeamAdapter extends ArrayAdapter<FirstTeam> {
         TextView country = (TextView) row.findViewById(R.id.countryTxt);
         TextView introduction = (TextView) row.findViewById(R.id.introductionTxt);
 
-        backNum.setText(data.getBackNum());
+        backNum.setText(data.getBackNum()+"");
         name.setText(data.getName());
         playerPosition.setText(data.getPosition());
         playedGame.setText(data.getPlayedGame());
